db8193dd1dc2a22d9e00d23ea7a7a19aefdd6fbd
Yash Poojary <yashspoojary245@gmail.com>
2021-09-23 18:46:56 +0530

    Adding custom animations via modifiers

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -7,10 +7,95 @@
 
 import SwiftUI
 
+
+struct CornerRotateModifier: ViewModifier {
+    
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+
+extension AnyTransition {
+    static var pivot: AnyTransition{
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+        
+    }
+    
+}
+
+
+
+
 struct ContentView: View {
+    
+    @State private var isShowingRed = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            
+            if isShowingRed {
+                Color.red
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+                   
+            }
+        
+        }
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+struct SecondaryView: View {
+    
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { index in
+                Text(String(letters[index]))
+                    .padding(5)
+                    .font(.title)
+                    .background( enabled ? Color.blue : Color.red)
+                    .offset(dragAmount)
+                    .animation(Animation.default.delay(Double(index) / 20 ))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged {dragAmount = $0.translation}
+                .onEnded {
+                    _ in
+                    
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+     
     }
 }
 
@@ -19,3 +104,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
